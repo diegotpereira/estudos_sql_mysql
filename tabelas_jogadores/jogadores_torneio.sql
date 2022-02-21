8c44604de67695b5bf99f1b0c996e27108437a7e
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-02-21 08:17:12 -0300

    criação dos exemplos de functions e triggers.

--- tabelas_jogadores/jogadores_torneio.sql
+++ tabelas_jogadores/jogadores_torneio.sql
@@ -17,3 +17,21 @@ UPDATE torneios SET vencedor_id = '4' WHERE id = '1';
 INSERT INTO jogadores_torneio (torneio_id, jogador_id) VALUES
 ('2', '4'), ('2', '11'), ('2', '1'), ('2', '12'), ('2', '15'), ('2', '3');
 UPDATE torneios SET vencedor_id = '11' WHERE id = '2';
+
+INSERT INTO jogadores_torneio (torneio_id, jogador_id) VALUES
+('3', '13'), ('3', '17'), ('3', '7'), ('3', '11'), ('3', '4');
+UPDATE torneios SET vencedor_id = '13' WHERE id = '3';
+
+INSERT INTO jogadores_torneio (torneio_id, jogador_id) VALUES
+('4', '2'), ('4', '7'), ('4', '8'), ('4', '10'), ('4', '16'), ('4', '20');
+UPDATE torneios SET vencedor_id = '7' WHERE id = '4';
+
+
+INSERT INTO jogadores_torneio (torneio_id, jogador_id) VALUES
+('5', '2'), ('5', '11'), ('5', '14'), ('5', '10'), ('5', '13'), ('5', '3'), ('5', '9');
+UPDATE torneios SET vencedor_id = '13' WHERE id = '5';
+
+INSERT INTO jogadores_torneio (torneio_id, jogador_id) VALUES
+('6', '20'), ('6', '17'), ('6', '14'), ('6', '1');
+UPDATE torneios SET vencedor_id = '17' WHERE id = '6';
+
